8e98f4dc67c2f47976704403fe93fa14bf9be1d9
webcubed from home <lainathannlaiv2@gmail.com>
2025-04-14 17:23:29 -0400

    forgot a space

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ app.use((request, resource, next) => {
 	);
 	next();
 });
-const whitedlistedEmails = new Set(
-	Array.from(process.env.WHITELISTED_EMAILS.split(","))
+const whitedlistedEmails = Array.from(
+	process.env.WHITELISTED_EMAILS.split(", ")
 );
+
 // Will get reset every time redeployed.
 const storage = {
 	accounts: {},
