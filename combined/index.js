d3d450991bc21b3671c286456525c1afddafb7ae
webcubed from home <lainathannlaiv2@gmail.com>
2025-06-05 12:01:02 -0400

    need baseurl to parse smh

--- combined/index.js
+++ combined/index.js
@@ -641,7 +641,8 @@ app.get("/check", async (request, response) => {
 });
 
 const authenticate = async (request) => {
-	const { account, code } = new URL(request.url, true).searchParams;
+	const baseURL = request.protocol + "://" + request.headers.host + "/";
+	const { account, code } = new URL(request.url, baseURL).searchParams;
 	const storage = structuredClone(await getStorage());
 	if (code !== storage.accounts[account].code) {
 		return false;
