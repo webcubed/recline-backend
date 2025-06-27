11165f12c96210eee4872f65fb7ee6818b542f25
webcubed <77589042+webcubed@users.noreply.github.com>
2025-06-27 17:33:16 -0400

    intent problem?

--- combined/index.js
+++ combined/index.js
@@ -20,6 +20,8 @@ const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.MessageContent,
 	],
 });
